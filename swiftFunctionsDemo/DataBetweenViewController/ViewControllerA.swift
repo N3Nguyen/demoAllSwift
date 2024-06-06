4838eef96284b5a126a3731dc88e085039039d2f
QuocTN <>
2024-06-06 19:52:07 +0700

    update document and demo code thread

---
+++ swiftFunctionsDemo/DataBetweenViewController/ViewControllerA.swift
@@ -0,0 +1,18 @@
+//
+//  ViewControllerA.swift
+//  swiftFunctionsDemo
+//
+//  Created by QuocTN on 06/06/2024.
+//
+
+import UIKit
+
+class ViewControllerA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
---
+++ swiftFunctionsDemo/DataBetweenViewController/ViewControllerB.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ViewControllerB: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
