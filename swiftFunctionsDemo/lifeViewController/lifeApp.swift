4d5eba9b956bbcaa3456008a4235ff9dd20be254
QuocTN <>
2024-06-22 09:31:25 +0700

    update function LifeCycle

---
+++ swiftFunctionsDemo/lifeViewController/lifeApp.swift
@@ -0,0 +1,33 @@
+// Ứng dụng đã được khởi động nhưng chưa sẵn sàng cho người dùng
+
+
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Khởi tạo những tài nguyên cần thiết hoặc thực hiện các thiết lập
+//        return true
+//    }
+//
+//    // Ứng dụng sắp chuyển từ trạng thái hoạt động sang không hoạt động
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        // Tạm dừng các tác vụ đang thực hiện, vô hiệu hóa bộ đếm thời gian, tạm dừng trò chơi
+//    }
+//
+//    // Ứng dụng đã chuyển sang trạng thái nền
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        // Giải phóng tài nguyên chia sẻ, lưu dữ liệu, hủy bỏ các yêu cầu mạng nếu cần thiết
+//    }
+//
+//    // Ứng dụng sắp chuyển từ nền sang hoạt động
+//    func applicationWillEnterForeground(_ application: UIApplication) {
+//        // Hoàn nguyên các thay đổi được thực hiện khi vào nền
+//    }
+//
+//    // Ứng dụng đã trở lại trạng thái hoạt động và sẵn sàng cho người dùng
+//    func applicationDidBecomeActive(_ application: UIApplication) {
+//        // Khởi động lại các tác vụ bị tạm dừng (hoặc chưa bắt đầu) khi ứng dụng không hoạt động
+//    }
+//
+//    // Ứng dụng sắp bị tắt
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        // Lưu dữ liệu nếu cần, giải phóng tài nguyên
+//    }
+
